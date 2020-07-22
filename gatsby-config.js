75eab651320e13ebd2cfc85351ecaab7b4964c4e
Jeongtae Kim <rsatang5@naver.com>
2020-07-22 14:44:42 +0900

    Install gatsby-source-filesystem and gatsby-transformer-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
     title: "정태 블로그",
     description: "김정태의 블로그입니다.",
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-styled-components`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+          name: `posts`,
+          path: `${__dirname}/posts`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 };
