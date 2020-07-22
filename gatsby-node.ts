f68f4c2e69a85ca55f6c3f9a88ae499f502e9fc4
Jeongtae Kim <rsatang5@naver.com>
2020-07-22 13:59:43 +0900

    Setup gatsby-node in Typescript

---
+++ gatsby-node.ts
@@ -0,0 +1,6 @@
+import { CreatePagesArgs, SourceNodesArgs } from "gatsby";
+
+export async function createPages({ actions }: CreatePagesArgs) {
+}
+export async function sourceNodes({ actions }: SourceNodesArgs) {
+}
