baade7e04c55f7e916ba45f0308e9b8474b3d867
Jeongtae Kim <rsatang5@naver.com>
2020-07-23 00:46:21 +0900

    Setup codes to create node field and pages

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,6 +1,47 @@
-import { CreatePagesArgs, SourceNodesArgs } from "gatsby";
+import { resolve } from "path";
+import { CreatePagesArgs, CreateNodeArgs } from "gatsby";
+import { createFilePath } from "gatsby-source-filesystem";
 
-export async function createPages({ actions }: CreatePagesArgs) {
+export async function createPages({ actions: { createPage }, graphql }: CreatePagesArgs) {
+  const { data, errors } = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+  if (errors) {
+    throw errors;
+  }
+
+  const posts = (data as any).allMarkdownRemark.edges;
+  const postTemplate = resolve("./src/templates/post.jsx");
+  for (const { node } of posts) {
+    createPage({
+      path: node.fields.slug,
+      context: {
+        slug: node.fields.slug,
+      },
+      component: postTemplate,
+    });
+  }
 }
-export async function sourceNodes({ actions }: SourceNodesArgs) {
+
+export function onCreateNode({ node, actions: { createNodeField }, getNode }: CreateNodeArgs) {
+  if (node.internal.type === "MarkdownRemark") {
+    const path = createFilePath({ node, getNode }).normalize("NFC").replace("\\", "/");
+    const paths = path.split("/").filter(x => x);
+    const slug = "/" + paths.pop();
+    createNodeField({
+      node: node,
+      name: "slug",
+      value: slug,
+    });
+  }
 }
