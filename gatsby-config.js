fec0db0b2a32497b31d52b822a230158ba95c413
Jeongtae Kim <rsatang5@naver.com>
2020-07-23 07:24:08 +0900

    Move posts to contents directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
           name: `posts`,
-          path: `${__dirname}/posts`,
+          path: `${__dirname}/contents/posts/`,
       },
     },
     `gatsby-transformer-remark`,
