9c3898b3044f92be0c02524362722df50b4a8d2a
Jeongtae Kim <rsatang5@naver.com>
2020-07-23 13:32:56 +0900

    Apply wordpress theme to typography config file

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -1,11 +1,18 @@
+import { CSSProperties } from "react";
 import oc from "open-color";
 import Typography from "typography";
+import wordpressTheme from "typography-theme-wordpress-2014";
 
+interface Style {
+  [selector: string]: CSSProperties;
+}
 
 export default new Typography({
+  ...wordpressTheme,
   baseFontSize: "18px",
   baseLineHeight: 1.5,
   bodyColor: oc.gray[9],
+  googleFonts: [],
   headerFontFamily: [
     "-apple-system",
     "BlinkMacSystemFont",
@@ -33,4 +40,9 @@ export default new Typography({
   ],
   bodyWeight: 300,
   boldWeight: 600,
+  overrideThemeStyles({ rhythm }, options) {
+    return {
+      "h1,h2": { color: "red" },  // test
+    } as Style;
+  },
 });
