ad14e5b67f66b36464a46249f8f333b0958a099c
Jeongtae Kim <rsatang5@naver.com>
2020-07-23 13:31:45 +0900

    Create basic typography config file

---
+++ src/utils/typography.ts
@@ -0,0 +1,36 @@
+import oc from "open-color";
+import Typography from "typography";
+
+
+export default new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.5,
+  bodyColor: oc.gray[9],
+  headerFontFamily: [
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "Segoe UI",
+    "Roboto",
+    "Oxygen",
+    "Ubuntu",
+    "Cantarell",
+    "Open Sans",
+    "Helvetica Neue",
+    "sans-serif",
+  ],
+  headerWeight: 600,
+  bodyFontFamily: [
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "Segoe UI",
+    "Roboto",
+    "Oxygen",
+    "Ubuntu",
+    "Cantarell",
+    "Open Sans",
+    "Helvetica Neue",
+    "sans-serif",
+  ],
+  bodyWeight: 300,
+  boldWeight: 600,
+});
