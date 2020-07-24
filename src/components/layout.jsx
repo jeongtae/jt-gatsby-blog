fdf73726d491275935e1d6ccbab16a1d4dc0a6c9
Jeongtae Kim <rsatang5@naver.com>
2020-07-24 14:42:09 +0900

    Style header and footer

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
+import oc from "open-color";
 import styled, { GlobalStyle, Breaks, css } from "../utils/styled-components";
 
 const Main = styled.main`
@@ -12,6 +13,59 @@ const Main = styled.main`
       }
     `
   )}
+`;
+const Header = styled.header`
+  position: sticky;
+  top: 0;
+  height: 3.8rem;
+  height: calc(env(safe-area-inset-top, 0) + 3.8rem);
+  margin: 0;
+  padding: 0;
+  padding-top: env(safe-area-inset-top, 0);
+  padding-left: env(safe-area-inset-left, 0);
+  padding-right: env(safe-area-inset-right, 0);
+  border-bottom: 1px solid ${oc.gray[3]};
+  background-color: ${oc.white};
+  backdrop-filter: blur(3px);
+  opacity: 0.8;
+  transition: transform linear 150ms;
+  transform: translateY(0%);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  &.hidden {
+    transform: translateY(-100%);
+  }
+`;
+const Footer = styled.footer`
+  text-align: center;
+  font-size: 0.8rem;
+  font-weight: 300;
+  color: ${oc.gray[6]};
+  border-top: 1px solid $oc-gray-3;
+  ul {
+    margin: 1rem 0 2rem;
+    padding-bottom: env(safe-area-inset-bottom, 0);
+    padding-left: env(safe-area-inset-left, 0);
+    padding-right: env(safe-area-inset-right, 0);
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    li {
+      display: block;
+      &::after {
+        content: "•";
+        margin: 0.2rem;
+      }
+      &:last-child::after {
+        content: "";
+      }
+    }
+  }
+  a {
+    text-decoration: underline;
+  }
 `;
 
 const Layout = ({ children }) => {
@@ -27,17 +81,15 @@ const Layout = ({ children }) => {
   return (
     <>
       <GlobalStyle />
-      <header>{data.site.siteMetadata.title}</header>
-      <footer>
+      <Header>{data.site.siteMetadata.title}</Header>
       <Main>{children}</Main>
+      <Footer className="sans">
         <ul>
-          <li>
-            &copy; {new Date().getFullYear()} Jeongtae Kim, All Rights Reserved
-          </li>
+          <li>&copy; {new Date().getFullYear()} Jeongtae Kim, All Rights Reserved</li>
           <li>Built with Gatsby</li>
           <li>Open Source Software Notice</li>
         </ul>
-      </footer>
+      </Footer>
     </>
   );
 };
