91ac7d89ee61dd2bdb31d2a3ff7a0133b2677119
Jeongtae Kim <rsatang5@naver.com>
2020-08-09 12:04:21 +0900

    Style post title and description

--- src/templates/Post.tsx
+++ src/templates/Post.tsx
@@ -1,8 +1,39 @@
 import React from "react";
 import { PageProps, graphql } from "gatsby";
+import styled, { ApplyBreaks, css } from "../utils/styled-components";
 import { MarkdownRemarkFrontmatter } from "../generated/graphql-types";
 import Layout from "../components/Layout";
 import MarkdownSection from "../components/MarkdownSection";
+
+const Title = styled.h1`
+  margin: 1.5rem 0 0.75rem;
+  font-size: 2.2rem;
+  font-weight: 500;
+  ${ApplyBreaks(
+    px => css`
+      margin: 2.5rem 0 1rem;
+      text-align: center;
+      font-size: 3rem;
+      font-weight: 700;
+    `,
+    ["sm"]
+  )};
+`;
+
+const Description = styled.p`
+  margin: 0.75rem 0.5rem 2rem;
+  color: $oc-gray-7;
+  font-size: 0.9rem;
+  font-weight: 300;
+  ${ApplyBreaks(
+    px => css`
+      margin: 1.5rem 10% 1rem;
+      text-align: center;
+      font-size: 1rem;
+    `,
+    ["sm"]
+  )};
+`;
 
 type PageData = {
   site: {
@@ -22,9 +53,8 @@ const PostTemplate: React.FC<PageProps<PageData>> = ({ data }) => {
   const { site, post } = data;
   return (
     <Layout navigationProps={{ title: post.frontmatter.title }}>
-      <h1>{site.siteMetadata.title}</h1>
-      <h2>{post.frontmatter.title}</h2>
-      <p>{post.excerpt}</p>
+      <Title>{post.frontmatter.title}</Title>
+      <Description>{post.excerpt}</Description>
       <time>{post.frontmatter.date}</time>
       <MarkdownSection html={post.html} />
     </Layout>
