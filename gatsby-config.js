cacc6b44473fa3fcf2832b9793b1b2d9ca8026e4
Jeongtae Kim <rsatang5@naver.com>
2020-08-13 11:20:48 +0900

    Install sitemap plugin and robots-txt plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,51 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-lodash`,
+    {
+      resolve: `gatsby-plugin-robots-txt`,
+      sitemap: `${siteUrl}/sitemap.xml`,
+      options: {
+        host: siteUrl,
+        policy: [{ userAgent: `*`, allow: [`/`] }],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/sitemap.xml`,
+        exclude: [`/search`],
+        query: `
+          query {
+            allSitePage {
+              nodes {
+                path
+                context {
+                  slug
+                }
+              }
+            }
+          }
+        `,
+        resolveSiteUrl() {
+          return siteUrl;
+        },
+        serialize({ allSitePage }) {
+          return allSitePage.nodes.map(node => {
+            const url = siteUrl + node.path;
+            if (node.path === `/`) {
+              // Root
+              return { url, changefreq: `daily`, priority: 0.3 };
+            } else if (node.context.slug) {
+              // Posts
+              return { url, changefreq: `weekly`, priority: 0.7 };
+            } else {
+              // Portfolio, About Me, ...
+              return { url, changefreq: `daily`, priority: 0.5 };
+            }
+          });
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     {
@@ -27,9 +72,9 @@ module.exports = {
     `gatsby-transformer-remark`,
     {
       // automatically generate typings from graphql schema
-      resolve: "gatsby-plugin-generate-typings",
+      resolve: `gatsby-plugin-generate-typings`,
       options: {
-        dest: "./src/generated/graphql-types.d.ts",
+        dest: `./src/generated/graphql-types.d.ts`,
       },
     },
   ],
