57f3f043bad432b645b26af58314959edcf87517
Jeongtae Kim <rsatang5@naver.com>
2020-08-13 12:42:01 +0900

    Install gatsby-image, sharp plugin and sharp transformer

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,6 +65,15 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `images`,
+        path: `${__dirname}/contents/images/`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `posts`,
         path: `${__dirname}/contents/posts/`,
       },
